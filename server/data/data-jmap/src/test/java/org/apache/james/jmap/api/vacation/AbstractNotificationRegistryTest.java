4f4bcb4f8545f94748e1a2c5058a70914dbfb274
Benoit Tellier <btellier@linagora.com>
2016-04-20 14:51:49 +0700

    JAMES-1717 Implement a memory Notification Registry

--- server/data/data-jmap/src/test/java/org/apache/james/jmap/api/vacation/AbstractNotificationRegistryTest.java
+++ server/data/data-jmap/src/test/java/org/apache/james/jmap/api/vacation/AbstractNotificationRegistryTest.java
@@ -37,7 +37,8 @@ public abstract class AbstractNotificationRegistryTest {
 
     public static final ZonedDateTime ZONED_DATE_TIME = ZonedDateTime.parse("2016-04-03T02:01:01+07:00[Asia/Vientiane]");
     public static final ZonedDateTime ZONED_DATE_TIME_PLUS_1_SECONDS = ZonedDateTime.parse("2016-04-03T02:01:02+07:00[Asia/Vientiane]");
-    public static final AccountId ACCOUNT_ID = AccountId.create("id");
+    public static final ZonedDateTime ZONED_DATE_TIME_PLUS_10_SECONDS = ZonedDateTime.parse("2016-04-03T02:01:11+07:00[Asia/Vientiane]");
+    public static final AccountId ACCOUNT_ID = AccountId.fromString("id");
     private NotificationRegistry notificationRegistry;
     private ZonedDateTimeProvider zonedDateTimeProvider;
     private RecipientId recipientId;
@@ -48,7 +49,7 @@ public abstract class AbstractNotificationRegistryTest {
     public void setUp() throws Exception {
         zonedDateTimeProvider = mock(ZonedDateTimeProvider.class);
         notificationRegistry = createNotificationRegistry(zonedDateTimeProvider);
-        recipientId = RecipientId.create(new MailAddress("benwa@apache.org"));
+        recipientId = RecipientId.fromMailAddress(new MailAddress("benwa@apache.org"));
     }
 
     @Test
@@ -77,6 +78,8 @@ public abstract class AbstractNotificationRegistryTest {
 
         notificationRegistry.register(ACCOUNT_ID, recipientId, Optional.of(ZONED_DATE_TIME_PLUS_1_SECONDS)).join();
 
+        when(zonedDateTimeProvider.get()).thenReturn(ZONED_DATE_TIME_PLUS_10_SECONDS);
+
         await().atMost(2, TimeUnit.SECONDS).until(() -> !notificationRegistry.isRegistered(ACCOUNT_ID, recipientId).join());
     }
 
@@ -91,14 +94,14 @@ public abstract class AbstractNotificationRegistryTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void question() throws Exception {
+    public void registerShouldThrowWhenExpiryDateIsPast() throws Exception {
         when(zonedDateTimeProvider.get()).thenReturn(ZONED_DATE_TIME_PLUS_1_SECONDS);
 
         notificationRegistry.register(ACCOUNT_ID, recipientId, Optional.of(ZONED_DATE_TIME)).join();
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void question1() throws Exception {
+    public void registerShouldThrowWhenExpiryDateIsPresent() throws Exception {
         when(zonedDateTimeProvider.get()).thenReturn(ZONED_DATE_TIME);
 
         notificationRegistry.register(ACCOUNT_ID, recipientId, Optional.of(ZONED_DATE_TIME)).join();
