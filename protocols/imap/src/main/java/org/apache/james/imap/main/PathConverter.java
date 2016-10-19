7eea957dfcd7204d53935bb30b9f6d1c14331815
Benoit Tellier <btellier@linagora.com>
2016-10-19 17:17:26 +0200

    JAMES-1838 Deny shared mailbox accesses

--- protocols/imap/src/main/java/org/apache/james/imap/main/PathConverter.java
+++ protocols/imap/src/main/java/org/apache/james/imap/main/PathConverter.java
@@ -77,6 +77,9 @@ public class PathConverter {
     }
 
     private MailboxPath buildMailboxPath(String namespace, String user, String mailboxName) {
+        if (!namespace.equals(MailboxConstants.USER_NAMESPACE)) {
+            throw new DeniedSharedMailboxException();
+        }
         return new MailboxPath(namespace, user, sanitizeMailboxName(mailboxName));
     }
 
