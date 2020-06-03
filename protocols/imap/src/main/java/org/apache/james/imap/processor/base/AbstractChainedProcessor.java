9288ae65cddc1355ee9ed62fe76b08762ec69689
Benoit Tellier <btellier@linagora.com>
2020-06-03 17:27:29 +0700

    fixup! JAMES-3196 IMAP Debug logs should allow reviewing issued commands

--- protocols/imap/src/main/java/org/apache/james/imap/processor/base/AbstractChainedProcessor.java
+++ protocols/imap/src/main/java/org/apache/james/imap/processor/base/AbstractChainedProcessor.java
@@ -54,7 +54,7 @@ public abstract class AbstractChainedProcessor<M extends ImapMessage> implements
             M acceptableMessage = (M) message;
             try (Closeable closeable = addContextToMDC(acceptableMessage)) {
                 try {
-                    LOGGER.debug("Processing {}", message);
+                    LOGGER.debug("Processing {}", message.toString());
                     doProcess(acceptableMessage, responder, session);
                 } catch (RuntimeException e) {
                     LOGGER.error("Error while processing IMAP request", e);
