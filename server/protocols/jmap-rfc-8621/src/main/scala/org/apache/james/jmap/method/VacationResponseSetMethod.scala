5ed0698b2241c903232bd407536e1c812d09934f
Benoit Tellier <btellier@linagora.com>
2020-08-31 15:16:34 +0700

    Fix compilation issue

--- server/protocols/jmap-rfc-8621/src/main/scala/org/apache/james/jmap/method/VacationResponseSetMethod.scala
+++ server/protocols/jmap-rfc-8621/src/main/scala/org/apache/james/jmap/method/VacationResponseSetMethod.scala
@@ -37,16 +37,16 @@ import org.reactivestreams.Publisher
 import play.api.libs.json.{JsError, JsObject, JsSuccess}
 import reactor.core.scala.publisher.SMono
 
-sealed trait UpdateResult {
+sealed trait VacationResponseUpdateResult {
   def updated: Option[Map[String, VacationResponseUpdateResponse]]
   def notUpdated: Option[Map[String, VacationResponseSetError]]
 }
-case class UpdateSuccess() extends UpdateResult {
+case class VacationResponseUpdateSuccess() extends VacationResponseUpdateResult {
   override def updated: Option[Map[String, VacationResponseUpdateResponse]] = Some(Map(VACATION_RESPONSE_PATCH_OBJECT_KEY -> VacationResponseUpdateResponse(JsObject(Seq()))))
 
   override def notUpdated: Option[Map[String, VacationResponseSetError]] = None
 }
-case class UpdateFailure(exception: Throwable) extends UpdateResult {
+case class VacationResponseUpdateFailure(exception: Throwable) extends VacationResponseUpdateResult {
   override def updated: Option[Map[String, VacationResponseUpdateResponse]] = None
 
   override def notUpdated: Option[Map[String, VacationResponseSetError]] = Some(Map(VACATION_RESPONSE_PATCH_OBJECT_KEY -> asSetError(exception)))
@@ -77,12 +77,12 @@ class VacationResponseSetMethod @Inject() (serializer: Serializer,
         .map(updateResult => createResponse(invocation, vacationResponseSetRequest, updateResult))))
   }
 
-  private def update(mailboxSession: MailboxSession, vacationResponseSetRequest: VacationResponseSetRequest): SMono[UpdateResult] = {
+  private def update(mailboxSession: MailboxSession, vacationResponseSetRequest: VacationResponseSetRequest): SMono[VacationResponseUpdateResult] = {
     vacationResponseSetRequest.parsePatch()
       .flatMap(patch => patch.asVacationPatch)
-      .fold(e => SMono.just(UpdateFailure(e)),
+      .fold(e => SMono.just(VacationResponseUpdateFailure(e)),
         patch => SMono.fromPublisher(vacationRepository.modifyVacation(toVacationAccountId(mailboxSession), patch))
-          .`then`(SMono.just(UpdateSuccess())))
+          .`then`(SMono.just(VacationResponseUpdateSuccess())))
   }
 
   private def toVacationAccountId(mailboxSession: MailboxSession): AccountId = {
@@ -98,7 +98,7 @@ class VacationResponseSetMethod @Inject() (serializer: Serializer,
 
   private def createResponse(invocation: Invocation,
                              vacationResponseSetRequest: VacationResponseSetRequest,
-                             updateResult: UpdateResult): Invocation = {
+                             updateResult: VacationResponseUpdateResult): Invocation = {
     val response = VacationResponseSetResponse(
       accountId = vacationResponseSetRequest.accountId,
       newState = State.INSTANCE,
