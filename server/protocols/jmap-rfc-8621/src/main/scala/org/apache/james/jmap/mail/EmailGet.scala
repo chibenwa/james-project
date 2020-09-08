93947bd1e4f164818b19453446592524f7212fff
Benoit Tellier <btellier@linagora.com>
2020-09-08 13:37:52 +0700

    [NO_REVIEW] #3761 JAMES-3369 Email/get bodyValues & fetchTextBodyValues + fetchHTMLBodyValues + fetchAllBodyValues + truncation

--- server/protocols/jmap-rfc-8621/src/main/scala/org/apache/james/jmap/mail/EmailGet.scala
+++ server/protocols/jmap-rfc-8621/src/main/scala/org/apache/james/jmap/mail/EmailGet.scala
@@ -19,16 +19,97 @@
 
 package org.apache.james.jmap.mail
 
-import org.apache.james.jmap.mail.Email.UnparsedEmailId
+import java.nio.charset.StandardCharsets.US_ASCII
+
+import cats.implicits._
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.auto._
+import eu.timepit.refined.numeric.NonNegative
+import org.apache.james.jmap.mail.Email.{UnparsedEmailId, sanitizeSize}
+import org.apache.james.jmap.mail.EmailGetRequest.MaxBodyValueBytes
 import org.apache.james.jmap.model.State.State
 import org.apache.james.jmap.model.{AccountId, Properties}
+import org.apache.james.mailbox.model.{MessageId, MessageResult}
+import org.apache.james.mime4j.codec.DecodeMonitor
+import org.apache.james.mime4j.dom.Header
+import org.apache.james.mime4j.message.DefaultMessageBuilder
+import org.apache.james.mime4j.stream.MimeConfig
+import scala.jdk.CollectionConverters._
+
+import scala.util.{Failure, Success, Try}
 
 case class EmailIds(value: List[UnparsedEmailId])
 
+object EmailGetRequest {
+  type MaxBodyValueBytes = Int Refined NonNegative
+
+  val ZERO: MaxBodyValueBytes = 0
+}
+
 case class EmailGetRequest(accountId: AccountId,
                            ids: Option[EmailIds],
+                           fetchAllBodyValues: Option[Boolean],
+                           fetchTextBodyValues: Option[Boolean],
+                           fetchHTMLBodyValues: Option[Boolean],
+                           maxBodyValueBytes: Option[MaxBodyValueBytes],
                            properties: Option[Properties],
-                           bodyProperties: Option[Properties])
+                           bodyProperties: Option[Properties]) {
+  def toEmail(message: (MessageId, Seq[MessageResult])): Try[Email] = {
+    val defaultMessageBuilder = new DefaultMessageBuilder
+    defaultMessageBuilder.setMimeEntityConfig(MimeConfig.PERMISSIVE)
+    defaultMessageBuilder.setDecodeMonitor(DecodeMonitor.SILENT)
+    val messageId: MessageId = message._1
+
+    for {
+      firstMessage <- message._2
+        .headOption
+        .map(Success(_))
+        .getOrElse(Failure(new IllegalArgumentException("No message supplied")))
+      mime4JMessage <- Try(defaultMessageBuilder.parseMessage(firstMessage.getFullContent.getInputStream))
+      bodyStructure <- EmailBodyPart.of(messageId, mime4JMessage)
+      bodyValues <- extractBodyValues(bodyStructure)
+    } yield {
+      Email(
+        id = messageId,
+        size = sanitizeSize(firstMessage.getSize),
+        bodyStructure = bodyStructure,
+        textBody = bodyStructure.textBody,
+        htmlBody = bodyStructure.htmlBody,
+        attachments = bodyStructure.attachments,
+        headers = asEmailHeaders(mime4JMessage.getHeader),
+        bodyValues = bodyValues)
+    }
+  }
+
+  def extractBodyValues(bodyStructure: EmailBodyPart): Try[Map[PartId, EmailBodyValue]] = for {
+    textBodyValues <- extractBodyValues(bodyStructure.textBody, fetchTextBodyValues.getOrElse(false))
+    htmlBodyValues <- extractBodyValues(bodyStructure.htmlBody, fetchHTMLBodyValues.getOrElse(false))
+    allBodyValues <- extractBodyValues(bodyStructure.flatten, fetchAllBodyValues.getOrElse(false))
+  } yield {
+    (textBodyValues ++ htmlBodyValues ++ allBodyValues)
+      .distinctBy(_._1)
+      .toMap
+  }
+
+  private def extractBodyValues(parts: List[EmailBodyPart], shouldFetch: Boolean): Try[List[(PartId, EmailBodyValue)]] =
+    if (shouldFetch) {
+      parts
+        .map(part => part.bodyContent.map(bodyValue => bodyValue.map(b => (part.partId, b.truncate(maxBodyValueBytes)))))
+        .sequence
+        .map(list => list.flatten)
+    } else {
+      Success(Nil)
+    }
+
+  private def asEmailHeaders(header: Header): List[EmailHeader] =
+    header.iterator()
+      .asScala
+      .map(header => EmailHeader(
+        EmailHeaderName(header.getName),
+        EmailHeaderValue(new String(header.getRaw.toByteArray, US_ASCII)
+          .substring(header.getName.length + 1))))
+      .toList
+}
 
 case class EmailNotFound(value: Set[UnparsedEmailId]) {
   def merge(other: EmailNotFound): EmailNotFound = EmailNotFound(this.value ++ other.value)
