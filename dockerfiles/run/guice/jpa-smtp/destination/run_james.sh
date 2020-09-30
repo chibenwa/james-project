34ad8519c50620e6e2544761c23154792169846e
Benoit Tellier <btellier@linagora.com>
2020-09-30 15:27:23 +0700

    fixup! JAMES-1717 AutomaticallySentMailDetector should handle auto-generated and auto-notified

--- dockerfiles/run/guice/jpa-smtp/destination/run_james.sh
+++ dockerfiles/run/guice/jpa-smtp/destination/run_james.sh
@@ -2,7 +2,12 @@
 if [ "$GLOWROOT_ACTIVATED" == "true" ]; then
     GLOWROOT_OPTIONS=-javaagent:/root/glowroot/glowroot.jar
 fi
-java -Dlogback.configurationFile=/root/conf/logback.xml \
+
+java -classpath '/root/james-server-jpa-smtp-guice.jar:/root/james-server-jpa-smtp-guice.lib/*' \
+  -javaagent:/root/james-server-jpa-smtp-guice.lib/openjpa-3.1.0.jar \
+  $GLOWROOT_OPTIONS \
+  -Dlogback.configurationFile=/root/conf/logback.xml \
   -Dworking.directory=/root/ \
   -Djdk.tls.client.protocols="TLSv1,TLSv1.1,TLSv1.2" \
-  $JVM_OPTIONS $GLOWROOT_OPTIONS -jar james-server.jar
+  $JVM_OPTIONS \
+   org.apache.james.JPAJamesServerMain
