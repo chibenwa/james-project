701a95a870d8f862d3bb99b36dbb0c3cb8e25879
Benoit Tellier <btellier@linagora.com>
2020-09-30 10:36:54 +0700

    JAMES-3394 Set guice docker jdk.tls.client.protocols to "TLSv1,TLSv1.1,TLSv1.2"
    This is intended to solve `javax.net.ssl.SSLException: Session has no PSK` and
    relax the default TLS set up of Java 8 (TLSv1.2)

--- dockerfiles/run/guice/cassandra-rabbitmq-ldap/destination/run_james.sh
+++ dockerfiles/run/guice/cassandra-rabbitmq-ldap/destination/run_james.sh
@@ -2,4 +2,7 @@
 if [ "$GLOWROOT_ACTIVATED" == "true" ]; then
     GLOWROOT_OPTIONS=-javaagent:/root/glowroot/glowroot.jar
 fi
-java -Dlogback.configurationFile=/root/conf/logback.xml -Dworking.directory=/root/ $JVM_OPTIONS $GLOWROOT_OPTIONS -jar james-server.jar
+java -Dlogback.configurationFile=/root/conf/logback.xml \
+  -Dworking.directory=/root/ \
+  -Djdk.tls.client.protocols="TLSv1,TLSv1.1,TLSv1.2" \
+  $JVM_OPTIONS $GLOWROOT_OPTIONS -jar james-server.jar
--- dockerfiles/run/guice/jpa/destination/run_james.sh
+++ dockerfiles/run/guice/jpa/destination/run_james.sh
@@ -2,4 +2,7 @@
 if [ "$GLOWROOT_ACTIVATED" == "true" ]; then
     GLOWROOT_OPTIONS=-javaagent:/root/glowroot/glowroot.jar
 fi
-java -classpath '/root/james-server.jar:/root/james-server-jpa-guice.lib/*' -javaagent:/root/james-server-jpa-guice.lib/openjpa-3.1.0.jar $GLOWROOT_OPTIONS -Dlogback.configurationFile=/root/conf/logback.xml -Dworking.directory=/root/ $JVM_OPTIONS org.apache.james.JPAJamesServerMain
+java -Dlogback.configurationFile=/root/conf/logback.xml \
+  -Dworking.directory=/root/ \
+  -Djdk.tls.client.protocols="TLSv1,TLSv1.1,TLSv1.2" \
+  $JVM_OPTIONS $GLOWROOT_OPTIONS -jar james-server.jar
