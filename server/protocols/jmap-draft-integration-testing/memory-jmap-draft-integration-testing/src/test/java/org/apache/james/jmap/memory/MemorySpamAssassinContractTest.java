9aa43d61d784ac4834cea673522ee32f819fef2e
Benoit Tellier <btellier@linagora.com>
2020-12-08 16:48:51 +0700

    fixup! JAMES-3466 Provision 'system' mailboxes only when listing mailboxes

--- server/protocols/jmap-draft-integration-testing/memory-jmap-draft-integration-testing/src/test/java/org/apache/james/jmap/memory/MemorySpamAssassinContractTest.java
+++ server/protocols/jmap-draft-integration-testing/memory-jmap-draft-integration-testing/src/test/java/org/apache/james/jmap/memory/MemorySpamAssassinContractTest.java
@@ -18,7 +18,14 @@
  ****************************************************************/
 package org.apache.james.jmap.memory;
 
-/*
+import org.apache.james.JamesServerBuilder;
+import org.apache.james.JamesServerExtension;
+import org.apache.james.MemoryJamesServerMain;
+import org.apache.james.jmap.draft.methods.integration.SpamAssassinContract;
+import org.apache.james.jmap.draft.methods.integration.SpamAssassinModuleExtension;
+import org.apache.james.modules.TestJMAPServerModule;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
 class MemorySpamAssassinContractTest implements SpamAssassinContract {
     @RegisterExtension
     static JamesServerExtension testExtension = new JamesServerBuilder<>(JamesServerBuilder.defaultConfigurationProvider())
@@ -26,4 +33,4 @@ class MemorySpamAssassinContractTest implements SpamAssassinContract {
         .server(configuration -> MemoryJamesServerMain.createServer(configuration)
             .overrideWith(new TestJMAPServerModule()))
         .build();
-}*/
+}
