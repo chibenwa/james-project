00c29e7bb29f25c3be29398ae3092867699e3d81
Benoit Tellier <btellier@linagora.com>
2021-06-04 14:35:47 +0700

    Rabbit module

--- server/container/guice/queue/rabbitmq/src/main/java/org/apache/james/modules/queue/rabbitmq/RabbitMQModule.java
+++ server/container/guice/queue/rabbitmq/src/main/java/org/apache/james/modules/queue/rabbitmq/RabbitMQModule.java
@@ -83,6 +83,8 @@ public class RabbitMQModule extends AbstractModule {
         bind(CassandraMailQueueBrowser.class).in(Scopes.SINGLETON);
         bind(CassandraMailQueueMailDelete.class).in(Scopes.SINGLETON);
         bind(CassandraMailQueueMailStore.class).in(Scopes.SINGLETON);
+        bind(SimpleConnectionPool.class).in(Scopes.SINGLETON);
+        bind(RabbitMQHealthCheck.class).in(Scopes.SINGLETON);
 
         Multibinder<CassandraModule> cassandraModuleBinder = Multibinder.newSetBinder(binder(), CassandraModule.class);
         cassandraModuleBinder.addBinding().toInstance(CassandraMailQueueViewModule.MODULE);
