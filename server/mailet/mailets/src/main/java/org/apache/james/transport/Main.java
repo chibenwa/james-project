e405def13180e939e8f263d142d6b76b55fdf197
Benoit Tellier <btellier@linagora.com>
2023-04-08 16:53:09 +0700

    Set up CSV
    Processes users concurrently

--- server/mailet/mailets/src/main/java/org/apache/james/transport/Main.java
+++ server/mailet/mailets/src/main/java/org/apache/james/transport/Main.java
@@ -1,5 +1,8 @@
 package org.apache.james.transport;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.security.KeyManagementException;
@@ -16,6 +19,8 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.apache.james.util.ClassLoaderUtils;
 
 import com.google.common.base.Stopwatch;
@@ -37,59 +42,76 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class Main {
-    private static final String PREFIX = "folder-k-";
+    private static final String PREFIX = "folder-m-";
     private static final Random RANDOM = new Random();
-    private static final String URL = "imaps://172.25.0.2:993";
-    private static int NUM_MBX = 10;
-    private static int NUM_MSG_REGULAR_FOLDER = 50;
-    private static int NUM_MSG_INBOX = 200;
-    private static final int NUM_OF_THREADS = 10;
     private static final byte[] HEADER_BYTES = ClassLoaderUtils.getSystemResourceAsByteArray("eml/headers");
     private static final byte[] HEADER_BYTES_SIMPLE = ClassLoaderUtils.getSystemResourceAsByteArray("eml/headersSimple");
     private static final byte[] BODY_START_BYTES = ClassLoaderUtils.getSystemResourceAsByteArray("eml/bodyStart");
     private static final byte[] BODY_END_BYTES = ClassLoaderUtils.getSystemResourceAsByteArray("eml/bodyEnd");
+    private static final byte[] CSV = ClassLoaderUtils.getSystemResourceAsByteArray("users.csv");
+
+    private static final String URL = "imaps://172.25.0.2:993";
+    private static final int NUM_MBX = 4;
+    private static final int NUM_MSG_REGULAR_FOLDER = 5;
+    private static final int NUM_MSG_INBOX = 10;
+    private static final int NUM_OF_THREADS = 10;
 
     public static void main(String... args) throws Exception {
-        // TODO get this from CSV, with limit, offset
-        String user = "bob@localhost";
-        String password = "123456";
+        Reader in = new InputStreamReader(new ByteArrayInputStream(CSV));
 
-        ImapAsyncClient imapClient = new ImapAsyncClient(NUM_OF_THREADS);
-        URI serverUri = new URI(URL);
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(in);
 
         final Stopwatch started = Stopwatch.createStarted();
 
-        // TODO pooling: for each user do 4 connections and mutualize requests to it
-        connect(imapClient, serverUri)
-
-            // LOGIN
-            .flatMap(session -> execute(session.getSession(), new LoginCommand(user, password))
-                .thenReturn(session.getSession()))
-
-            // Provision massages into INBOX
-            .flatMap(session -> Flux.range(0, NUM_MSG_INBOX)
-                .concatMap(j -> execute(session, new AppendCommand("INBOX", new Flags(), new Date(), messageBytes()))).then().thenReturn(session))
-
-            // Create mailboxes with sub messages
-            .flatMap(session -> Flux.range(0, NUM_MBX)
-                .concatMap(i -> execute(session, new CreateFolderCommand(PREFIX + i))
-                    .then(Flux.range(0, NUM_MSG_REGULAR_FOLDER)
-                        .concatMap(j -> execute(session, new AppendCommand(PREFIX + i, new Flags(), new Date(), messageBytes()))).then()))
-                .then())
+        Flux.fromIterable(records)
+            .flatMap(Main::provisionUser, 10)
+            .then()
             .block();
 
         System.out.println("Elapsed " + started.elapsed(TimeUnit.MILLISECONDS) + " ms");
     }
 
+    private static Mono<Void> provisionUser(CSVRecord record)  {
+        try {
+            ImapAsyncClient imapClient = new ImapAsyncClient(NUM_OF_THREADS);
+            URI serverUri = new URI(URL);
+
+
+            // TODO pooling: for each user do 4 connections and mutualize requests to it
+            return connect(imapClient, serverUri)
+
+                // LOGIN
+                .flatMap(session -> execute(session.getSession(), new LoginCommand(record.get(0), record.get(1)))
+                    .thenReturn(session.getSession()))
+
+                // Provision massages into INBOX
+                .flatMap(session -> Flux.range(0, NUM_MSG_INBOX)
+                    .concatMap(j -> execute(session, new AppendCommand("INBOX", new Flags(), new Date(), messageBytes()))).then().thenReturn(session))
+
+                // Create mailboxes with sub messages
+                .flatMap(session -> Flux.range(0, NUM_MBX)
+                    .concatMap(i -> execute(session, new CreateFolderCommand(PREFIX + i))
+                        .then(Flux.range(0, NUM_MSG_REGULAR_FOLDER)
+                            .concatMap(j -> execute(session, new AppendCommand(PREFIX + i, new Flags(), new Date(), messageBytes()))).then()))
+                    .then())
+
+                .then();
+        } catch(Exception e) {
+            return Mono.error(e);
+        }
+    }
+
     private static byte[] messageBytes() {
         int tirage = Math.abs(RANDOM.nextInt());
+        byte[] customHeader = ("\r\nX-CUSTOM: " + UUID.randomUUID().toString() + "\r\n\r\n").getBytes();
+        byte[] customBodyValue = UUID.randomUUID().toString().getBytes();
 
         // 1% -> 10MB   (with attachments)
         if (tirage % 100 == 0) {
             return Bytes.concat(
                 HEADER_BYTES,
-                ("\r\nX-CUSTOM: " + UUID.randomUUID().toString() + "\r\n\r\n").getBytes(),
-                UUID.randomUUID().toString().getBytes(),
+                customHeader,
+                customBodyValue,
                 BODY_START_BYTES,
                 // 10MB Attachment 66
                 Strings.repeat("UmV0dXJuLVBhdGg6IDx0cHJ1ZGVudG92YUBsaW5hZ29yYS5jb20+DQpvcmcuYXBhY2hlLmph\r\n", 151515).getBytes(),
@@ -99,8 +121,8 @@ public class Main {
         if (tirage % 100 < 4) {
             return Bytes.concat(
                 HEADER_BYTES,
-                ("\r\nX-CUSTOM: " + UUID.randomUUID().toString() + "\r\n\r\n").getBytes(),
-                UUID.randomUUID().toString().getBytes(),
+                customHeader,
+                customBodyValue,
                 BODY_START_BYTES,
                 // 10MB Attachment 66
                 Strings.repeat("UmV0dXJuLVBhdGg6IDx0cHJ1ZGVudG92YUBsaW5hZ29yYS5jb20+DQpvcmcuYXBhY2hlLmph\r\n", 75757).getBytes(),
@@ -110,8 +132,8 @@ public class Main {
         if (tirage % 100 < 10) {
             return Bytes.concat(
                 HEADER_BYTES,
-                ("\r\nX-CUSTOM: " + UUID.randomUUID().toString() + "\r\n\r\n").getBytes(),
-                UUID.randomUUID().toString().getBytes(),
+                customHeader,
+                customBodyValue,
                 BODY_START_BYTES,
                 // 10MB Attachment 66
                 Strings.repeat("UmV0dXJuLVBhdGg6IDx0cHJ1ZGVudG92YUBsaW5hZ29yYS5jb20+DQpvcmcuYXBhY2hlLmph\r\n", 15151).getBytes(),
@@ -121,8 +143,8 @@ public class Main {
         if (tirage % 100 < 20) {
             return Bytes.concat(
                 HEADER_BYTES,
-                ("\r\nX-CUSTOM: " + UUID.randomUUID().toString() + "\r\n\r\n").getBytes(),
-                UUID.randomUUID().toString().getBytes(),
+                customHeader,
+                customBodyValue,
                 BODY_START_BYTES,
                 // 10MB Attachment 66
                 Strings.repeat("UmV0dXJuLVBhdGg6IDx0cHJ1ZGVudG92YUBsaW5hZ29yYS5jb20+DQpvcmcuYXBhY2hlLmph\r\n", 1515).getBytes(),
@@ -130,8 +152,8 @@ public class Main {
         }
         return Bytes.concat(
             HEADER_BYTES_SIMPLE,
-            ("\r\nX-CUSTOM: " + UUID.randomUUID().toString() + "\r\n\r\n").getBytes(),
-            UUID.randomUUID().toString().getBytes());
+            customHeader,
+            customBodyValue);
     }
 
     static Mono<ImapAsyncCreateSessionResponse> connect(ImapAsyncClient imapClient, URI serverUri) throws NoSuchAlgorithmException, KeyManagementException {
